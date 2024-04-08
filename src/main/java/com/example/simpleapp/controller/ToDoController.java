d74d48ca47d65a4eace0d99305befb3e4a2600eb
sumilon <sumilon.4you@gmail.com>
2024-04-08 14:20:20 +0530

    new changes

--- src/main/java/com/example/simpleapp/controller/ToDoController.java
+++ src/main/java/com/example/simpleapp/controller/ToDoController.java
@@ -3,46 +3,38 @@ package com.example.simpleapp.controller;
 import com.example.simpleapp.model.ToDo;
 import com.example.simpleapp.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class ToDoController {
 
     @Autowired
     private ToDoService toDoService;
 
     @GetMapping("/")
-    public String getAllToDos(Model model) {
-        model.addAttribute("todos", toDoService.getAllTodos());
-        return "todos";
-    }
-
-    @GetMapping("/all")
-    public List<ToDo> getAll() {
+    public List<ToDo> getAllToDos() {
         return toDoService.getAllTodos();
     }
 
     @PostMapping("/add")
-    public String addToDo(@ModelAttribute ToDo todo) {
+    public String addToDo(@RequestBody ToDo todo) {
         toDoService.addOrUpdateToDo(todo);
-        return "redirect:/";
+        return "Success";
     }
 
     @PostMapping("/edit/{id}")
-    public String editToDo(@PathVariable String id, @ModelAttribute ToDo todo) {
+    public String editToDo(@PathVariable String id, @RequestBody ToDo todo) {
         todo.setId(id); // Set the ID for updating existing ToDo
         toDoService.addOrUpdateToDo(todo);
-        return "redirect:/";
+        return "Success";
     }
 
     @PostMapping("/delete/{id}")
     public String deleteToDo(@PathVariable String id) {
         toDoService.deleteToDoById(id);
-        return "redirect:/";
+        return "Success";
     }
 }
 
